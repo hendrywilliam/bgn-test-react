57a67e9086314eb27883c6ded18d6acf8d854619
hendrywilliam <hendriwilliam29@gmail.com>
2023-03-06 15:23:37 +0700

    ganti loading

--- src/components/PageLoading/PageLoading.jsx
+++ src/components/PageLoading/PageLoading.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Loading from "@/assets/images/loading.gif";
 
 export default function PageLoading() {
   return (
     <div className="page-loading-container">
       <div className="page-loading-image">
-        <img src={Loading} alt="" />
+        <img
+          src="https://lumiere-a.akamaihd.net/v1/images/sw_logo_stacked_2x-52b4f6d33087_7ef430af.png?region=0,0,586,254"
+          alt=""
+        />
       </div>
     </div>
   );
